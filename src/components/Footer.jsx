b79260f9852dc45de17c3e69d4358123fae30dca
JaiSuriya p k <80122325+jaisuriya97@users.noreply.github.com>
2024-10-22 04:17:04 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
       <div className='flex m-10 mt-52 dark:text-white text-black'>
         <div className='flex flex-col gap-3 text-md justify-center items-center p-4 cursor-pointer'>
-              <p>© 2023 Jaisuriya . All rights reserved.</p>
+              <p>© 2024 Jaisuriya . All rights reserved.</p>
               <p className='text-lg font-semibold'>Thank You 💀</p>
         </div>
     </div>

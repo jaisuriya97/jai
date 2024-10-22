8e9fc4aef1af1c43896380c70a3bf2525dfaafd9
jaisuriya97 <jaisuriya200297@gmail.com>
2024-10-22 09:25:30 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar'
 import Herocard from "./components/Herocard";
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -11,22 +10,8 @@ import Education from "./components/ui/Education";
 import Skill from "./components/ui/Skill";
 import DarkModeSwitch from "./components/ui/DarkModeSwitch";
 import About from "./components/ui/About";
-import './App.css';
-
-const useIsMobile = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  return isMobile;
-};
+import ScrollToTop from "./ScrollToTop";
+import './App.css'
 
 const LandingPage = () => {
   return (
@@ -36,47 +21,67 @@ const LandingPage = () => {
         <Herocard />
         <MainContent />
       </div>
-      <DarkModeSwitch />  
+      <DarkModeSwitch />
       <Footer />
     </div>
   );
 };
 
-const MainPage = ({ Component }) => {
-  const isMobile = useIsMobile();
-  const location = useLocation();
-
+const ProjectsPage = () => {
   return (
-    <div className='flex flex-col justify-center items-center text-white w-full px-4 lg:px-20'>
+    <div className='flex flex-col justify-center items-center text-white w-full  px-4 lg:px-20'>
       <Navbar />
-      <div className='flex flex-col xl:flex-row items-center xl:items-start md:items-center lg:items-center mt-20 gap-20 w-full justify-center relative'>
-        {!isMobile || location.pathname === '/' ? <Herocard /> : null}
+      <div className='flex flex-col xl:flex-row items-center xl:items-start md:items-center lg:items-center mt-20 gap-20 w-full justify-center relative' >
+        <Herocard />
         <div className="max-w-[700px]">
-          <Component />
+          <Projects />
           <Contact />
         </div>
       </div>
-      <DarkModeSwitch />  
+      <DarkModeSwitch />
       <Footer />
     </div>
-  );
-};
-
-const ProjectsPage = () => <MainPage Component={Projects} />;
-const ExperiencePage = () => <MainPage Component={Experience} />;
-const AboutPage = () => (
-  <MainPage Component={() => (
-    <>
-      <About />
-      <Education />
-      <Skill />
-    </>
-  )} />
-);
+  )
+}
+const ExperiencePage = () => {
+  return (
+    <div className='flex flex-col justify-center items-center text-white w-full  px-4 lg:px-20'>
+      <Navbar />
+      <div className='flex flex-col xl:flex-row items-center xl:items-start md:items-center lg:items-center mt-20 gap-20 w-full justify-center relative'>
+        <Herocard />
+        <div className="max-w-[700px]">
+          <Experience />
+          <Contact />
+        </div>
+      </div>
+      <DarkModeSwitch />
+      <Footer />
+    </div>
+  )
+}
+const AboutPage = () => {
+  return (
+    <div className='flex flex-col justify-center items-center text-white w-full  px-4 lg:px-20'>
+      <Navbar />
+      <div className='flex flex-col xl:flex-row items-center xl:items-start md:items-center lg:items-center mt-20 gap-20 w-full justify-center relative'>
+        <Herocard />
+        <div className="max-w-[700px]">
+          <About />
+          <Education />
+          <Skill />
+          <Contact />
+        </div>
+      </div>
+      <DarkModeSwitch />
+      <Footer />
+    </div>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path='/' element={<LandingPage />} />
         <Route path='/projects' element={<ProjectsPage />} />
@@ -84,7 +89,7 @@ function App() {
         <Route path='/about' element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
